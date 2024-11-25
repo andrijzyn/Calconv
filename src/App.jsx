fd6417d31fe56eea13f6033176a734d50f53c2ae
andrijzyn <zinukandrij@gmail.com>
2024-11-25 15:58:40 +0200

    Added bootstrap usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import Calculator from './Calculator';
 import Converter from './Converter';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 const App = () => {
-    return (
-        <div className="app">
-            <h2>Base Converter and Calculator</h2>
-            <div id="flex-container">
-                <Calculator />
-                <Converter />
-            </div>
+  return (
+    <div id="main-container" className="container-fluid d-flex vh-100">
+      <div className="row flex-grow-1">
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+          <div className="card shadow-sm window p-4">
+            <h3 className="card-title text-center" >Calculator</h3>
+            <Calculator />
+          </div>
         </div>
-    );
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+          <div className="card shadow-sm window p-4">
+            <h3 className="card-title text-center">Converter</h3>
+            <Converter />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
