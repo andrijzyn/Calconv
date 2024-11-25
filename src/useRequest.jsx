93c69329e69291ff6d9d1cf3b96012e7871be8e8
andrijzyn <zinukandrij@gmail.com>
2024-11-25 15:56:57 +0200

    Added alerting about Error

--- src/useRequest.jsx
+++ src/useRequest.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 const useRequest = () => {
     const [result, setResult] = useState('');
     const [steps, setSteps] = useState([]);
+    const [error, setError] = useState(null);
 
     const handleRequest = async (url, data) => {
+        setError(null);
         try {
             const response = await fetch(url, {
                 method: 'POST',
@@ -22,13 +24,19 @@ const useRequest = () => {
             setSteps(responseData.steps);
 
         } catch (error) {
-            console.error('Error during the request:', error);
+            const customError = {
+                message: 'Request failed',
+                code: 'SERVER_ERROR',
+                details: error.message,
+            };
+            setError(customError);
         }
     };
 
     return {
         result,
         steps,
+        error,
         handleRequest,
     };
 };
